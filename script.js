a7847e5184126d441da84b680daa1ed2e90478bd
Oliver Nygren <oliver.nygren@icloud.com>
2020-11-02 13:45:31 +0100

    added hamburger manu icon and basic code

---
+++ script.js
@@ -0,0 +1,12 @@
+// const header = document.querySelector('header');
+// const menuIcon = document.getElementById('menu');
+
+// menuIcon.onclick = function() {
+
+//     if (header.style.height == '100%') {
+//         header.style.height = null;
+//     } else {
+//         header.style.height = '100%';
+//     }
+    
+// }

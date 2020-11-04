eac4a66accae67ed0262eaa218f3874c03ac7cf6
Oliver Nygren <oliver.nygren@icloud.com>
2020-11-04 10:55:09 +0100

    JS applies bg-color to header on scroll, completed

--- script.js
+++ script.js
@@ -10,3 +10,15 @@
 //     }
     
 // }
+
+const header = document.querySelector('header');
+
+window.onscroll = function() {
+    let top = window.scrollY;
+
+    if (top >= 50) {
+        header.classList.add('active')
+    } else {
+        header.classList.remove('active');
+    }
+}
